ed003c9734e7c2cd6915eeb0a8bdf66197b014c0
Ardityo Giantra <agiantra123@gmail.com>
2018-11-16 14:52:17 +0700

    16-11-2018 add redux-persist

--- App.js
+++ App.js
@@ -1,27 +1,46 @@
 import React, {Component} from 'react';
+import {AsyncStorage} from 'react-native'
 
 import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 
-import {createLogger} from 'redux-logger' // Using RN Debugger
-import {composeWithDevTools} from 'redux-devtools-extension' // Using RN Debugger
+// For Redux Persist
+import {persistStore, persistReducer} from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+
+// For RN Debugger
+import {createLogger} from 'redux-logger' 
+import {composeWithDevTools} from 'redux-devtools-extension' 
 
 import rootReducer from './src/store/reducers/rootReducer'
 import Input from './src/containers/Input'
 
-const middlewares = []
+// For Redux Persist
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  // stateReconciler: autoMergeLevel1 // Default
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
+// For RN Debugger
+const middlewares = []
 if (__DEV__){
   middlewares.push(createLogger()) // Using RN Debugger
 }
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)))
+const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(...middlewares))) // For RN Debugger composeWithDevTools
+
+// For Redux Persist
+const persistor = persistStore(store)
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Input />
+        <PersistGate persistor={persistor} loading={null}>
+          <Input />
+        </PersistGate>
       </Provider>
     );
   }
